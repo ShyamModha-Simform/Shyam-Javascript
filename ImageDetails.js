06fe26944a2cc6b6992a9b7c879338af7f6bced9
ShyamModha-Simform <shyam.m@simformsolutions.com>
2023-04-04 10:49:57 +0530

    Separate out Static Data and JS Logic..

---
+++ ImageDetails.js
@@ -0,0 +1,46 @@
+const sliderImageDetails = [
+    {
+      imageURL:
+        "https://images.unsplash.com/photo-1680428517644-0f6569346521?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+      placeholder: "Squirrel zombie",
+      className: `slider-item slider-item-visible`,
+      radioButtonChecked: "checked",
+    },
+    {
+      imageURL:
+        "https://images.unsplash.com/photo-1503925802536-c9451dcd87b5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&h=450&q=80",
+      placeholder: "Squirrel zombie",
+      className: "slider-item",
+      radioButtonChecked: "",
+    },
+    {
+      imageURL:
+        "https://images.unsplash.com/photo-1509558567730-6c838437b06b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&h=450&q=80",
+      placeholder: "Squirrel zombie",
+      className: "slider-item",
+      radioButtonChecked: "",
+    },
+    {
+      imageURL:
+        "https://images.unsplash.com/photo-1509558567730-6c838437b06b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&h=450&q=80",
+      placeholder: "Squirrel zombie",
+      className: "slider-item",
+      radioButtonChecked: "",
+    },
+    {
+      imageURL:
+        "https://images.unsplash.com/photo-1509558567730-6c838437b06b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&h=450&q=80",
+      placeholder: "Squirrel zombie",
+      className: "slider-item",
+      radioButtonChecked: "",
+    },
+    {
+      imageURL:
+        "https://images.unsplash.com/photo-1509558567730-6c838437b06b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&h=450&q=80",
+      placeholder: "Squirrel zombie",
+      className: "slider-item",
+      radioButtonChecked: "",
+    },
+  ];
+
+  export default sliderImageDetails;
