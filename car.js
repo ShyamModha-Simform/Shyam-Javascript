cda1a2fdd3538dcf9639305f37e630c4ef47955a
ShyamModha-Simform <shyam.m@simformsolutions.com>
2023-04-04 10:58:06 +0530

    Added: Favicon for webpage...

--- car.js
+++ car.js
@@ -1,86 +1,46 @@
+// Dynmaic Data for images
+import sliderImageDetails from "./ImageDetails.js";
+
 // --------------Global DOM imports-------------
 // For Slider
-const sliderContainer = document.querySelector('.slider');
-const sliderDots = document.querySelector('.slider-dots');
+const sliderContainer = document.querySelector(".slider");
+const sliderDots = document.querySelector(".slider-dots");
 const slides = document.getElementsByClassName("slider-item");
 const nextButton = document.getElementById("slider-button-next");
 const prevButton = document.getElementById("slider-button-prev");
 const dots = document.getElementsByClassName("dot");
 let position = 0;
-// const numberOfSlides = slides.length;
-console.log()
-
-const sliderImageDetails = [
-  {
-    imageURL: "https://images.unsplash.com/photo-1537211261771-e525b9e4049b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&h=450&q=80",
-    placeholder: "Squirrel zombie",
-    className: `slider-item slider-item-visible`,
-    radioButtonChecked: "checked",
-  },
-  {
-    imageURL: "https://images.unsplash.com/photo-1503925802536-c9451dcd87b5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&h=450&q=80",
-    placeholder: "Squirrel zombie",
-    className: "slider-item",
-    radioButtonChecked: "",
-
-  },
-  {
-    imageURL: "https://images.unsplash.com/photo-1509558567730-6c838437b06b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&h=450&q=80",
-    placeholder: "Squirrel zombie",
-    className: "slider-item",
-    radioButtonChecked: "",
-  },
-  {
-    imageURL: "https://images.unsplash.com/photo-1509558567730-6c838437b06b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&h=450&q=80",
-    placeholder: "Squirrel zombie",
-    className: "slider-item",
-    radioButtonChecked: "",
-  },
-  {
-    imageURL: "https://images.unsplash.com/photo-1509558567730-6c838437b06b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&h=450&q=80",
-    placeholder: "Squirrel zombie",
-    className: "slider-item",
-    radioButtonChecked: "",
-  },
-  {
-    imageURL: "https://images.unsplash.com/photo-1509558567730-6c838437b06b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&h=450&q=80",
-    placeholder: "Squirrel zombie",
-    className: "slider-item",
-    radioButtonChecked: "",
-  }
-];
-
-const sliderItemMarkup = ({imageURL, placeholder, className}) => {
-  // console.log(className);
-  let div = document.createElement("div");
-  div.className = className;
-  div.innerHTML =  `<img src=${imageURL} alt=${placeholder}/>`;
-  sliderContainer.appendChild(div);
-  return ;
-};
-
-const sliderDotsMarkup = ({radioButtonChecked}) => {
-  console.log(`<input class="dot selected-dot" type="radio" name="dot" ${radioButtonChecked} />`, radioButtonChecked)
-  sliderDots.innerHTML += `<input class="dot selected-dot" type="radio" name="dot" ${radioButtonChecked} />`;
-  return ;
-};
-
-sliderImageDetails.forEach(ele => {
-  // console.log(typeof sliderItemMarkup(ele))
-  sliderItemMarkup(ele);
-  // console.log(sliderContainer, "Container");
-  
-  sliderDotsMarkup(ele);
-  console.log(sliderDots, "Dots");
-
-  // console.log("Adding Slider Images....", ele);
-});
 
 // For Scrolling
 let mybutton = document.getElementById("myBtn");
 const links = document.querySelectorAll(".nav-link");
 
-// -----------------Logic for image slider--------------
+// Dynamic input for Slider's Images
+
+
+
+const sliderItemMarkup = ({ imageURL, placeholder, className }) => {
+  let div = document.createElement("div");
+  div.className = className;
+  div.innerHTML = `<img src=${imageURL} alt="${placeholder}" cross-origin="anonymous"/>`;
+  sliderContainer.appendChild(div);
+  return;
+};
+
+// Dynamic Input for Slider dots or indicators
+const sliderDotsMarkup = ({ radioButtonChecked }) => {
+  sliderDots.innerHTML += `<input class="dot selected-dot" type="radio" name="dot" ${radioButtonChecked} />`;
+  return;
+};
+
+// Mapping Details array -- To display Images--------
+
+sliderImageDetails.forEach((ele) => {
+  sliderItemMarkup(ele);
+  sliderDotsMarkup(ele);
+});
+
+// -----------------Logic for image slider----------------------------------
 function hideAllSlides() {
   // remove all slides not currently being viewed
   for (const slide of slides) {
@@ -88,6 +48,26 @@ function hideAllSlides() {
     slide.classList.add("slider-item-hidden");
   }
 }
+
+const handleVisibility = (pos) => {
+  // make current slide visible
+  slides[pos].classList.add("slider-item-visible");
+
+  // update dot to represent current slide
+  dots[pos].classList.add("selected-dot");
+  dots[pos].checked = true;
+};
+
+const handleMoveForRadioButton = function (e) {
+  hideAllSlides();
+  const selectedRadioButton = document.querySelector(".dot:checked");
+
+  position = Array.from(selectedRadioButton.parentElement.children).indexOf(
+    selectedRadioButton
+  );
+  // Use to make appropriate image visible
+  handleVisibility(position);
+};
 
 const handleMoveToNextSlide = function (e) {
   hideAllSlides();
@@ -99,12 +79,8 @@ const handleMoveToNextSlide = function (e) {
     // move to next slide
     position++;
   }
-  // make current slide visible
-  slides[position].classList.add("slider-item-visible");
-
-  // update dot to represent current slide
-  dots[position].classList.add("selected-dot");
-  dots[position].checked = true;
+  // Use to make appropriate image visible
+  handleVisibility(position);
 };
 
 const handleMoveToPrevSlide = function (e) {
@@ -117,17 +93,14 @@ const handleMoveToPrevSlide = function (e) {
     // move back one
     position--;
   }
-  // make current slide visible
-  slides[position].classList.add("slider-item-visible");
-
-  // update dot to represent current slide
-  dots[position].classList.add("selected-dot");
-  dots[position].checked = true;
+  // Use to make appropriate image visible
+  handleVisibility(position);
 };
 
 // listen for slide change events
 nextButton.addEventListener("click", handleMoveToNextSlide);
 prevButton.addEventListener("click", handleMoveToPrevSlide);
+sliderDots.addEventListener("click", handleMoveForRadioButton);
 
 //------------ When the user scrolls down 300px from the top of the document, ----------
 // ---------------show the button, Scroll to top button---
@@ -146,11 +119,12 @@ function scrollFunction() {
     mybutton.style.display = "none";
   }
 }
-function topFunction() {
-  document.body.scrollTop = 0;
-}
 
-// ------------ Helper Function to scroll to any particular element------
+document.getElementById("myBtn").addEventListener("click", () => {
+  document.body.scrollTop = 0;
+})
+
+// ------------ Helper Function for scrolling to any particular element----------
 
 for (const ele of links) {
   // console.log(ele);
@@ -178,12 +152,13 @@ function findPosition(obj, navbarHeight) {
       currenttop += obj.offsetTop;
       console.log(currenttop);
       // offsetTop -- distance between outer border of current element to inner border of offsetParent Element
-
     } while ((obj = obj.offsetParent)); // offsetParent -- returns closest positioned ancestor element
 
     return [currenttop - navbarHeight];
   }
 }
+
+
 
 // document.querySelectorAll(".nav-link[data-custom-value]").forEach((element) => {
 //   element.addEventListener("click", function (event) {
